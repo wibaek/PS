7910eb91bd9aa9aac32a24de375780212064f2a9
devMuromi <devmuromi@gmail.com>
2023-05-01 19:19:29 +0900

    230501

---
+++ 2000/2445.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        for (int j = (n - i - 1) * 2; j > 0; j--) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        for (int j = (n - i - 1) * 2; j > 0; j--) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
