84a5022026ad7dc503ec2186259207e971a1b49d
devMuromi <devmuromi@gmail.com>
2024-03-26 09:50:37 +0900

    240326

---
+++ BOJ/9000/9215.py
@@ -0,0 +1,14 @@
+# 2024 - 03 - 26
+s1 = input()
+s2 = input()
+
+cache = [[0 for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
+
+for i in range(1, len(s1) + 1):
+    for j in range(1, len(s2) + 1):
+        if s1[i - 1] == s2[j - 1]:
+            cache[i][j] = cache[i - 1][j - 1] + 1
+        else:
+            cache[i][j] = max(cache[i - 1][j], cache[i][j - 1])
+
+print(cache[-1][-1])
